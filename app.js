e02118a64f2e8f3896f74cdb26490d16ebb35258
Alex Francis <alexanderjamesfrancis@hotmail.co.uk>
2022-08-16 10:44:44 +0100

    Complete Tailwin set up

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
-
+const path = require("path")
 
 const app = express()
 
 //View Engine and Use options
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.use(express.static('css'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }));
 
 
